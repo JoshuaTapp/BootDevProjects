394ccbf4a4ffc4f314ac8ebfaf55c98797f2c22d
Joshua Tapp <joshua.c.tapp@icloud.com>
2024-05-23 14:14:56 -0400

    finished catch cmd (#6)

---
+++ pokedexcli/internal/pokedex/pokedex.go
@@ -0,0 +1,35 @@
+package pokedex
+
+import (
+	"log"
+
+	"github.com/JoshuaTapp/BootDevProjects/pokedexcli/internal/pokeAPI"
+)
+
+var (
+	caughtPokemon map[string]pokeAPI.Pokemon
+)
+
+func init() {
+	caughtPokemon = make(map[string]pokeAPI.Pokemon)
+	log.Println("pokedex is online!")
+}
+
+func IsCaught(name string) bool {
+	if _, ok := caughtPokemon[name]; ok {
+		return true
+	}
+	return false
+}
+
+func AddPokemon(p pokeAPI.Pokemon) error {
+	name := p.Name
+	if IsCaught(name) {
+		log.Println(name, " already caught!")
+		return nil
+	}
+
+	caughtPokemon[name] = p
+	log.Println("Adding ", name, " to pokedex!")
+	return nil
+}
