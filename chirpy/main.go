c8b2e29afc987fb3b584816197c0f1dd084c37c0
JoshuaTapp <joshua.c.tapp@icloud.com>
2024-05-24 19:49:10 +0000

    ch 3 finished

--- chirpy/main.go
+++ chirpy/main.go
@@ -1,20 +1,78 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
+type apiConfig struct {
+	fileserverHits int
+}
+
 func main() {
 	const port = "8080"
+	cfg := new(apiConfig)
 
 	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("."))
 
+	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", fileServer)))
+	mux.HandleFunc("GET /admin/metrics", cfg.metricsHandler)
+	mux.HandleFunc("GET /api/reset", cfg.resetHandler)
+	mux.HandleFunc("GET /api/healthz", healthHandler)
+
+	loggingHandler := loggingMiddleware(mux)
 	srv := &http.Server{
 		Addr:    ":" + port,
-		Handler: mux,
+		Handler: loggingHandler,
 	}
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Metrics Called - Hits: ", cfg.fileserverHits)
+
+	tmpl := fmt.Sprintf(`
+	<html>
+		<body>
+			<h1>Welcome, Chirpy Admin</h1>
+			<p>Chirpy has been visited %d times!</p>
+		</body>
+	</html>
+	`, cfg.fileserverHits)
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(tmpl))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Resetting hits...")
+	cfg.fileserverHits = 0
+	w.WriteHeader(http.StatusOK)
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Print("middleware triggered - Prev Hits: ", cfg.fileserverHits)
+		cfg.fileserverHits++
+		log.Print("New Hits: ", cfg.fileserverHits)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Visited page: %s\n", r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
 }
