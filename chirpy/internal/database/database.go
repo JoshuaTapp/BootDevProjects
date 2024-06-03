14d59158a5c860f84ad1b4c0ae7c4f751f734260
JoshuaTapp <joshua.c.tapp@icloud.com>
2024-06-03 20:22:14 +0000

    auth done and big refactor

--- chirpy/internal/database/database.go
+++ chirpy/internal/database/database.go
@@ -1,294 +1,88 @@
 package database
 
 import (
-	"crypto/rand"
-	"encoding/hex"
 	"encoding/json"
 	"errors"
-	"io"
-	"log"
 	"os"
 	"sync"
-
-	"golang.org/x/crypto/bcrypt"
 )
+
+var ErrNotExist = errors.New("resource does not exist")
 
 type DB struct {
 	path string
-	mux  *sync.RWMutex
+	mu   *sync.RWMutex
 }
 
 type DBStructure struct {
-	Chirps map[int]Chirp `json:"chirps"`
-	Users  map[int]User  `json:"users"`
+	Chirps        map[int]Chirp           `json:"chirps"`
+	Users         map[int]User            `json:"users"`
+	RefreshTokens map[string]RefreshToken `json:"refresh_tokens"`
 }
 
-type User struct {
-	ID           int    `json:"id"`
-	Email        string `json:"email"`
-	Password     []byte `json:"password"`
-	RefreshToken string `json:"refresh_token"`
-}
-
-type Chirp struct {
-	Body string `json:"body"`
-	ID   int    `json:"id"`
-}
-
-// NewDB creates a new database connection
-// and creates the database file if it doesn't exist
-func NewDB(path string, debug bool) (*DB, error) {
-	db := DB{
+func NewDB(path string) (*DB, error) {
+	db := &DB{
 		path: path,
-		mux:  new(sync.RWMutex),
+		mu:   &sync.RWMutex{},
 	}
-	log.Printf("creating database: %v", db)
 	err := db.ensureDB()
-	if err != nil {
-		return nil, err
-	}
-	if debug {
-		// if debug, overwrite the file!
-		initDB := new(DBStructure)
-		initDB.Chirps = make(map[int]Chirp)
-		initDB.Users = make(map[int]User)
-		err = db.writeDB(*initDB)
-		return &db, err
-	}
-
-	return &db, err
+	return db, err
 }
 
-func (db *DB) CreateUser(email, password string) (u User, err error) {
-	dbs, err := db.loadDB()
-	if err != nil {
-		return
+func (db *DB) createDB() error {
+	dbStructure := DBStructure{
+		Chirps:        map[int]Chirp{},
+		Users:         map[int]User{},
+		RefreshTokens: map[string]RefreshToken{},
 	}
-	newID := len(dbs.Users) + 1
-	hashPW, err := bcrypt.GenerateFromPassword([]byte(password), 10) // default cost
-	if err != nil {
-		log.Fatal("password hashing failed!", password, hashPW)
-	}
-	u = User{
-		Email:    email,
-		ID:       newID,
-		Password: hashPW,
-	}
-
-	dbs.Users[newID] = u
-	err = db.writeDB(dbs)
-	if err != nil {
-		return User{}, err
-	}
-
-	return
+	return db.writeDB(dbStructure)
 }
 
-func (db *DB) GetUser(email string) (u User, err error) {
-	dbs, err := db.loadDB()
-	if err != nil {
-		return u, err
-	}
-
-	// this is dumb implementation, i should use emails instead for key...
-	for _, v := range dbs.Users {
-		if v.Email == email {
-			u = v
-			break
-		}
-	}
-	if u.Email == "" {
-		err = errors.New("user not found")
-	}
-
-	return
-}
-
-func (db *DB) GetUserById(id int) (u User, err error) {
-	dbs, err := db.loadDB()
-	if err != nil {
-		return u, err
-	}
-
-	u, ok := dbs.Users[id]
-	if !ok {
-		return User{}, errors.New("user not found")
-	}
-
-	return u, nil
-}
-
-func (db *DB) UpdateUser(id int, user User) (User, error) {
-	userOld, err := db.GetUserById(id)
-	if err != nil {
-		return userOld, err
-	}
-
-	if user.Email != "" {
-		userOld.Email = user.Email
-	}
-	if len(user.Password) > 0 {
-		hashPW, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10) // default cost
-		if err != nil {
-			log.Fatal("password hashing failed!", user.Password, hashPW)
-		}
-		user.Password = hashPW
-	}
-	user.ID = id
-
-	dbs, err := db.loadDB()
-	if err != nil {
-		return userOld, err
-	}
-
-	dbs.Users[id] = user
-	err = db.writeDB(dbs)
-	if err != nil {
-		return userOld, err
-	}
-
-	return user, nil
-}
-
-// CreateChirp creates a new chirp and saves it to disk
-func (db *DB) CreateChirp(body string) (c Chirp, err error) {
-	dbs, err := db.loadDB()
-	if err != nil {
-		return
-	}
-	newID := len(dbs.Chirps) + 1
-	c = Chirp{
-		Body: body,
-		ID:   newID,
-	}
-
-	dbs.Chirps[newID] = c
-	err = db.writeDB(dbs)
-	if err != nil {
-		return Chirp{}, err
-	}
-
-	return
-}
-
-// GetChirps returns all chirps in the database
-func (db *DB) GetChirps() (chirps []Chirp, err error) {
-	dbs, err := db.loadDB()
-	if err != nil {
-		log.Printf("failed to load during getChirps: %v", dbs)
-		return
-	}
-
-	for _, chirp := range dbs.Chirps {
-		chirps = append(chirps, chirp)
-	}
-
-	log.Println("GOT CHIRPS:", chirps, err)
-
-	return
-}
-
-// GetChirps returns all chirps in the database
-func (db *DB) GetChirp(ID int) (chirp Chirp, err error) {
-	dbs, err := db.loadDB()
-	if err != nil {
-		log.Printf("failed to load during getChirps: %v", dbs)
-		return
-	}
-
-	if _, ok := dbs.Chirps[ID]; !ok {
-		return chirp, errors.New("chirp does not exist")
-	}
-
-	log.Printf("requested ID: %v |\tChirp: %v", ID, dbs.Chirps[ID])
-	return dbs.Chirps[ID], nil
-
-}
-
-// ensureDB creates a new database file if it doesn't exist
 func (db *DB) ensureDB() error {
-	dbFile, err := os.OpenFile(db.path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+	_, err := os.ReadFile(db.path)
+	if errors.Is(err, os.ErrNotExist) {
+		return db.createDB()
+	}
+	return err
+}
+
+func (db *DB) ResetDB() error {
+	err := os.Remove(db.path)
+	if errors.Is(err, os.ErrNotExist) {
+		return nil
+	}
+	return db.ensureDB()
+}
+
+func (db *DB) loadDB() (DBStructure, error) {
+	db.mu.RLock()
+	defer db.mu.RUnlock()
+
+	dbStructure := DBStructure{}
+	dat, err := os.ReadFile(db.path)
+	if errors.Is(err, os.ErrNotExist) {
+		return dbStructure, err
+	}
+	err = json.Unmarshal(dat, &dbStructure)
 	if err != nil {
-		log.Print("Failed ensureDB check!")
+		return dbStructure, err
+	}
+
+	return dbStructure, nil
+}
+
+func (db *DB) writeDB(dbStructure DBStructure) error {
+	db.mu.Lock()
+	defer db.mu.Unlock()
+
+	dat, err := json.Marshal(dbStructure)
+	if err != nil {
 		return err
 	}
-	defer dbFile.Close()
 
+	err = os.WriteFile(db.path, dat, 0600)
+	if err != nil {
+		return err
+	}
 	return nil
-}
-
-// loadDB reads the database file into memory
-func (db *DB) loadDB() (dbs DBStructure, err error) {
-	db.mux.RLock()
-	defer db.mux.RUnlock()
-
-	dbFile, err := os.Open(db.path)
-	if err != nil {
-		return
-	}
-	defer dbFile.Close()
-
-	byteVal, err := io.ReadAll(dbFile)
-	if err != nil {
-		return
-	}
-
-	err = json.Unmarshal(byteVal, &dbs)
-	if err != nil {
-		return
-	}
-
-	return dbs, nil
-
-}
-
-// writeDB writes the database file to disk
-func (db *DB) writeDB(dbs DBStructure) error {
-	db.mux.Lock()
-	defer db.mux.Unlock()
-
-	dbFile, err := os.OpenFile(db.path, os.O_RDWR|os.O_TRUNC, 0644)
-	if err != nil {
-		return err
-	}
-	defer dbFile.Close()
-
-	byteVal, err := json.Marshal(dbs)
-	if err != nil {
-		return err
-	}
-
-	_, err = dbFile.Write(byteVal)
-	if err != nil {
-		return err
-	}
-
-	return nil
-
-}
-
-func (db *DB) refreshUserRefreshToken(userID int) error {
-	// generate new random 32 byte token
-	b := make([]byte, 32)
-	_, err := rand.Read(b)
-	if err != nil {
-		log.Print("error refreshing token: ", err)
-		return err
-	}
-	token := hex.EncodeToString(b)
-
-	// assign new token to user
-	dbs, err := db.loadDB()
-	if err != nil {
-		return err
-	}
-
-	if u, ok := dbs.Users[userID]; ok {
-		u.RefreshToken = token
-		dbs.Users[userID] = u
-		return db.writeDB(dbs)
-	} 
-	
-	return errors.New("can not refresh token because user does not exist")
-	
 }
--- chirpy/main.go
+++ chirpy/main.go
@@ -1,396 +1,77 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
 	"flag"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
-	"strings"
-	"time"
 
 	"github.com/JoshuaTapp/BootDevProjects/chirpy/internal/database"
-	"github.com/golang-jwt/jwt/v5"
 	"github.com/joho/godotenv"
-	"golang.org/x/crypto/bcrypt"
 )
 
 type apiConfig struct {
 	fileserverHits int
-	jwtSecret      []byte
-	db             *database.DB
+	DB             *database.DB
+	jwtSecret      string
 }
-
-var (
-	cfg apiConfig
-)
-
-const accessTokenInterval int = 60 * 60            // 1 hour
-const refreshTokenInterval int = 60 * 24 * 60 * 60 // 60 Days
 
 func main() {
+	const filepathRoot = "."
+	const port = "8080"
+
+	godotenv.Load(".env")
+
+	jwtSecret := os.Getenv("JWT_SECRET")
+	if jwtSecret == "" {
+		log.Fatal("JWT_SECRET environment variable is not set")
+	}
+
+	db, err := database.NewDB("database.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	dbg := flag.Bool("debug", false, "Enable debug mode")
 	flag.Parse()
-
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	cfg.jwtSecret = []byte(os.Getenv("JWT_SECRET"))
-	log.Print(cfg.jwtSecret)
-	cfg.db, _ = database.NewDB("database.json", *dbg)
-
-	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("."))
-
-	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", fileServer)))
-	mux.HandleFunc("GET /admin/metrics", cfg.metricsHandler)
-	mux.HandleFunc("GET /api/reset", cfg.resetHandler)
-	mux.HandleFunc("GET /api/healthz", healthHandler)
-	mux.HandleFunc("GET /api/chirps", getChirpHandler)
-	mux.HandleFunc("GET /api/chirps/{chirpID}", getChirpHandler)
-
-	mux.HandleFunc("POST /api/chirps", postChirpHandler)
-	mux.HandleFunc("POST /api/users", postUsersHandler)
-	mux.HandleFunc("POST /api/login", postLoginHandler)
-	mux.HandleFunc("POST /api/refresh", refreshTokenHandler)
-	mux.HandleFunc("POST /api/revoke", revokeTokenHandler)
-
-	mux.HandleFunc("PUT /api/users", putUsersHandler)
-
-	loggingHandler := loggingMiddleware(mux)
-	const port = "8080"
-	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: loggingHandler,
-	}
-
-	log.Printf("Serving on port: %s\n", port)
-	log.Fatal(srv.ListenAndServe())
-}
-
-func healthHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))
-}
-
-func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Metrics Called - Hits: ", cfg.fileserverHits)
-
-	tmpl := fmt.Sprintf(`
-	<html>
-		<body>
-			<h1>Welcome, Chirpy Admin</h1>
-			<p>Chirpy has been visited %d times!</p>
-		</body>
-	</html>
-	`, cfg.fileserverHits)
-
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(tmpl))
-}
-
-func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Resetting hits...")
-	cfg.fileserverHits = 0
-	w.WriteHeader(http.StatusOK)
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Print("middleware triggered - Prev Hits: ", cfg.fileserverHits)
-		cfg.fileserverHits++
-		log.Print("New Hits: ", cfg.fileserverHits)
-		next.ServeHTTP(w, r)
-	})
-}
-
-func loggingMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("Visited page: %v - %s\n", r.Method, r.URL.Path)
-		next.ServeHTTP(w, r)
-	})
-}
-
-func validateChirp(msg string) (bool, error) {
-	if len(msg) > 140 {
-		return false, errors.New("Chirp is too long")
-	}
-
-	return true, nil
-}
-
-func removeProfanity(msg string) string {
-	theProfane := map[string]string{
-		"kerfuffle": "****",
-		"sharbert":  "****",
-		"fornax":    "****",
-	}
-
-	strSlice := strings.Split(msg, " ")
-	log.Print("strSlice: ", strSlice)
-
-	for i, s := range strSlice {
-		lcStr := strings.ToLower(s)
-		if v, ok := theProfane[lcStr]; ok {
-			strSlice[i] = v
+	if dbg != nil && *dbg {
+		err := db.ResetDB()
+		if err != nil {
+			log.Fatal(err)
 		}
 	}
 
-	return strings.Join(strSlice, " ")
-
-}
-
-func respondWithError(w http.ResponseWriter, code int, msg string) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
-
-	respBody := struct {
-		Error string `json:"error"`
-	}{
-		Error: msg,
+	apiCfg := apiConfig{
+		fileserverHits: 0,
+		DB:             db,
+		jwtSecret:      jwtSecret,
 	}
 
-	rb, err := json.Marshal(&respBody)
-	if err != nil {
-		log.Printf("Error mashalling JSON: %s", err)
-		return
-	}
-	w.Write(rb)
-}
+	mux := http.NewServeMux()
+	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/*", fsHandler)
 
-func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("GET /api/reset", apiCfg.handlerReset)
 
-	rb, err := json.Marshal(&payload)
-	if err != nil {
-		log.Printf("Error mashalling JSON: %s", err)
-		return
-	}
-	w.Write(rb)
-}
+	mux.HandleFunc("POST /api/revoke", apiCfg.handlerRevoke)
+	mux.HandleFunc("POST /api/refresh", apiCfg.handlerRefresh)
+	mux.HandleFunc("POST /api/login", apiCfg.handlerLogin)
 
-func decodeJSON(r *http.Request, shape interface{}) error {
-	decoder := json.NewDecoder(r.Body)
-	return decoder.Decode(shape)
-}
+	mux.HandleFunc("POST /api/users", apiCfg.handlerUsersCreate)
+	mux.HandleFunc("PUT /api/users", apiCfg.handlerUsersUpdate)
 
-func postChirpHandler(w http.ResponseWriter, r *http.Request) {
-	c := &struct {
-		Body string `json:"body"`
-	}{}
-	err := decodeJSON(r, c)
-	if err != nil {
-		log.Println("Error decoding params: ", err)
-		respondWithError(w, http.StatusInternalServerError, "Something went wrong")
-		return
+	mux.HandleFunc("POST /api/chirps", apiCfg.handlerChirpsCreate)
+	mux.HandleFunc("GET /api/chirps", apiCfg.handlerChirpsRetrieve)
+	mux.HandleFunc("GET /api/chirps/{chirpID}", apiCfg.handlerChirpsGet)
+
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
 	}
 
-	if ok, err := validateChirp(c.Body); !ok {
-		respondWithError(w, http.StatusBadRequest, err.Error())
-		return
-	}
-
-	chirp, err := cfg.db.CreateChirp(removeProfanity(c.Body))
-	if err != nil {
-		log.Print("failed to create chirp", err)
-		return
-	}
-	respondWithJSON(w, http.StatusCreated, chirp)
-}
-
-func getChirpHandler(w http.ResponseWriter, r *http.Request) {
-	var chirps []database.Chirp
-
-	chirps, err := cfg.db.GetChirps()
-	if err != nil {
-		log.Print("failure getting chirps", err)
-	}
-
-	id, err := strconv.Atoi(r.PathValue("chirpID"))
-	if err != nil {
-		respondWithJSON(w, http.StatusOK, chirps)
-		return
-	}
-
-	c, err := cfg.db.GetChirp(id)
-	if err != nil {
-		respondWithError(w, 404, "chirp not found")
-		return
-	}
-	respondWithJSON(w, http.StatusOK, c)
-}
-
-func postUsersHandler(w http.ResponseWriter, r *http.Request) {
-	u := &struct {
-		Email    string `json:"email"`
-		Password string `json:"password"`
-	}{}
-
-	err := decodeJSON(r, u)
-	if err != nil {
-		log.Println("Error decoding params: ", err)
-		respondWithError(w, http.StatusInternalServerError, "Something went wrong")
-		return
-	}
-
-	user, err := cfg.db.CreateUser(u.Email, u.Password)
-	if err != nil {
-		log.Print("failed to create user", err)
-		return
-	}
-
-	// remove password field from response
-	response := struct {
-		Email string `json:"email"`
-		ID    int    `json:"id"`
-	}{
-		Email: user.Email,
-		ID:    user.ID,
-	}
-
-	respondWithJSON(w, 201, response)
-}
-
-func postLoginHandler(w http.ResponseWriter, r *http.Request) {
-	u := &struct {
-		Email    string `json:"email"`
-		Password string `json:"password"`
-		Expires  int    `json:"expires_in_seconds,omitempty"`
-	}{}
-
-	err := decodeJSON(r, u)
-	if err != nil {
-		log.Println("Error decoding params: ", err)
-		respondWithError(w, http.StatusInternalServerError, "Something went wrong")
-		return
-	}
-
-	user, err := cfg.db.GetUser(u.Email)
-	if err != nil {
-		log.Print("failed to login", err)
-		respondWithError(w, 401, "invalid user")
-		return
-	}
-
-	err = bcrypt.CompareHashAndPassword(user.Password, []byte(u.Password))
-	if err != nil {
-		// passwords do not match
-		respondWithError(w, 401, "invalid password")
-		return
-	}
-
-	if u.Expires < 1 || u.Expires >= accessTokenInterval {
-		u.Expires = accessTokenInterval
-	}
-
-	claims := &jwt.RegisteredClaims{
-		Issuer:    "chirpy",
-		IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
-		ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(time.Duration(u.Expires) * time.Second)),
-		Subject:   strconv.Itoa(user.ID),
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	ss, err := token.SignedString(cfg.jwtSecret)
-	if err != nil {
-		log.Print("signing failed!", err)
-		respondWithError(w, http.StatusInternalServerError, "signing failed")
-
-	}
-
-	// remove password field from response
-	response := struct {
-		Email        string `json:"email"`
-		ID           int    `json:"id"`
-		Token        string `json:"token"`
-		RefreshToken string `json:"refresh_token"`
-	}{
-		Email: user.Email,
-		ID:    user.ID,
-		Token: ss,
-		RefreshToken: "CHANGE", // CHANGE THIS TO CORRECT 
-	}
-
-	respondWithJSON(w, 200, response)
-}
-
-func putUsersHandler(w http.ResponseWriter, r *http.Request) {
-	// get token and verify auth
-
-	tokenString := r.Header.Get("Authorization")
-	if tokenString == "" {
-		log.Println("Error: token not provided")
-	}
-	tokenString = strings.TrimPrefix(tokenString, "Bearer ")
-	log.Println("Token String:", tokenString)
-
-	claims := &jwt.RegisteredClaims{}
-
-	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
-
-		return cfg.jwtSecret, nil
-	})
-	if err != nil {
-		log.Printf("failed to parse token: %v", err)
-		respondWithError(w, 401, "invalid token - parse error")
-		return
-	}
-
-	if !token.Valid {
-		respondWithError(w, 401, "invalid token")
-		return
-	}
-
-	id, err := token.Claims.GetSubject()
-	if err != nil {
-		respondWithError(w, 401, "no user id provided in jwt")
-		return
-	}
-
-	u := &struct {
-		Email    string `json:"email"`
-		Password string `json:"password"`
-	}{}
-	err = decodeJSON(r, u)
-	if err != nil {
-		log.Println("Error decoding params: ", err)
-		respondWithError(w, http.StatusInternalServerError, "Something went wrong")
-		return
-	}
-	id2, err := strconv.Atoi(id)
-	if err != nil {
-		respondWithError(w, 401, "invalid userID")
-	}
-	user := database.User{
-		Email:    u.Email,
-		Password: []byte(u.Password),
-		ID:       id2,
-	}
-
-	user, err = cfg.db.UpdateUser(id2, user)
-	// remove password field from response
-	response := struct {
-		Email string `json:"email"`
-		ID    int    `json:"id"`
-	}{
-		Email: user.Email,
-		ID:    user.ID,
-	}
-
-	respondWithJSON(w, 200, response)
-}
-
-func refreshTokenHandler(w http.ResponseWriter, r *http.Request) {
-
-}
-
-func revokeTokenHandler(w http.ResponseWriter, r *http.Request) {
-
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
 }
